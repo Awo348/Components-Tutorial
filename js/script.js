0f0978525079349fd7634baaba8c7eda873396cf
Adewole Samuel <samueladewole18@gmail.com>
2022-08-09 10:43:37 +0100

    First Commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// document.getElementById('orangediv').style.background = "black"
+
+// method 1
+// document.getElementById('orangedivtstfddffg').style.background = "#87CEEB"
+
+// ES 5
+
+var seconddiv = document.getElementById('orangediv')
+
+// poping an alert when clicked
+seconddiv.addEventListener('click', function () {
+    alert('orange div is clicked')
+})
+
+// changing the element backgroud
+seconddiv.style.background = "blue" 
+
+function popsomething() {
+    alert("Welcome, box 1 is now clicked")
+}
+
+var rightdiv = document.querySelector('#rightdiv')
